67310e9536025ea1d444ecce38ac030237e557de
major684 <lucas.velasquez@outlook.com>
2015-08-30 18:46:52 -0700

    Styled hompage with bootstrap
    using template to style home page.  have list of categories and latest 5
    items

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -4,7 +4,7 @@
 # Date: 8/25/2015
 
 # Import Flask
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 # Import DB
 from sqlalchemy import create_engine
@@ -17,15 +17,23 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-def hello_world():
-    categories = session.query(Categories).first()
+def homePage():
+    categories = session.query(Categories).all()
+    items = session.query(Items).join(Categories, Items.category_id == Categories.id) \
+    							.order_by(Items.date_added.desc()).limit(5) \
+    							.values(Items.item_name, Categories.category_name)
+    
+    return render_template('home.html', categories = categories, items = items)
+# Categories Route
+@app.route('/catalog/<string:category_name>/items/')
+def categoryItems(category_name):
+    categories = session.query(Categories).filter_by(category_name = category_name).one()
     items = session.query(Items).filter_by(category_id = categories.id)
     output = ''
     for i in items:
     	output += i.item_name
     	output += '</br>'
     return output
-
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
--- vagrant/catalog/seed_database.py
+++ vagrant/catalog/seed_database.py
@@ -19,7 +19,13 @@ sessionD.commit()
 session = DBSession()
 # Create a new category named Insturments
 newCategory = Categories(category_name = 'Instruments')
+secondCategory = Categories(category_name = 'Video Games')
+thirdCategory = Categories(category_name = 'Sports Equipment')
+fourthCategory = Categories(category_name = 'Board Games')
 session.add(newCategory)
+session.add(secondCategory)
+session.add(thirdCategory)
+session.add(fourthCategory)
 # Commit changes to database
 session.commit()
 # Add items for the category Instruments
@@ -28,6 +34,13 @@ newItem = Items(item_name = 'Guitar', description = 'Amazing guitar with persona
 session.add(newItem)
 newItem = Items(item_name = 'Ukulele', description = 'Keep it island with this beautiful Ukulele',
 				 category_id = newCategory.id)
+session.add(newItem)
+# Add items for the category Video Games
+newItem = Items(item_name = 'Fallout 4', description = 'Post-apocalyptic RPG set in Boston',
+				 category_id = secondCategory.id)
+session.add(newItem)
+newItem = Items(item_name = 'Star Wars Battlefront', description = 'Star Wars meet Battlefront',
+				 category_id = secondCategory.id)
 session.add(newItem)
 # Commit changes to database
 session.commit()
